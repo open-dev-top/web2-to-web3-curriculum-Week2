ca5170c7f65046e53cc6943cd346f05d78a0edc4
open dev <opendev.top@gmail.com>
2024-05-17 17:59:51 +0800

    Add day3/sendETH.js

---
+++ day3-v6/scripts/sendETH.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const hre = require("hardhat");
+const { ethers, BigNumber } = require("ethers");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const myBalance = await deployer.provider.getBalance(deployer.address);
+  console.log("My balance", myBalance);
+
+  const toAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+  console.log("Sending ETH to", toAddress);
+
+  const tx = await deployer.sendTransaction({
+    to: toAddress,
+    value: myBalance / 10n,
+  });
+
+  console.log("TX SENT!", tx.hash);
+  await tx.wait();
+  console.log("TX MINED!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.emit(1);
+  });
